64a92359fa031ba31c1c72f27ae02fd538363a76
Richard Schepen <9021227@student.zadkine.nl>
2025-10-15 09:35:15 +0200

    OpenStreetMap toegevoegd
    De enige stad is Rotterdam, ga dus niet kijken of Timboektoe bekend is :D

--- resources/views/Klachtenformulier.blade.php
+++ resources/views/Klachtenformulier.blade.php
@@ -5,8 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Klachtenpagina</title>
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <script src="/js/map.js" defer></script>
 </head>
 <body>
+
 <nav class="topBar">
 <li>
     <a href="/">Home</a>
@@ -15,24 +19,35 @@
 </nav>
     <nav class="formBody">
     <form action="">
-            <label for="email-adres">E-mail:</label><br>
-            <input type="email" id="email-adres" name="email-adres" required><br><br>
-            <label for="Naam">Naam:</label><br>
-            <input type="text" id="name" name="name" required><br><br>
-            <label for="Telefoonnummer">Telefoonnummer:</label><br>
-            <input  type="tel" name="Telefoonummer" id="Telefoonnummer" pattern="[0-9]{10}" required><br><br>
-             <label for="klacht" required>Type Klacht:</label><br>
-             <select name="klacht" id="klacht">
-                <option value="KiesKlacht" selected>-Kies uw klacht-</option>
-                <option value="Zwerfafval">Zwerfafval</option>
-                <option value="Dood_Dier">Dood dier</option>
-                <option value="Kapot_Stoplicht">Kapot Stoplicht</option>
-                <option value="Overig">Overig</option>
-                </select><br><br>
-                <label  for="Klacht" required>Uw klacht:</label><br>
-                 <textarea rows="4" cols="50">
-                </textarea><br><br>
-<input class="verzendknop" type="submit" value="Klacht Verzenden">
+            <form id="complaintForm" onsubmit="return false;"> <!-- Prevent default form submission -->
+    <label for="email-adres">E-mail:</label><br>
+    <input type="email" id="email-adres" name="email-adres" required><br><br>
+
+    <label for="name">Naam:</label><br>
+    <input type="text" id="name" name="name" required><br><br>
+
+    <label for="Telefoonnummer">Telefoonnummer:</label><br>
+    <input type="tel" name="Telefoonnummer" id="Telefoonnummer" pattern="[0-9]{10}" required><br><br>
+
+    <label for="streetName">Straatnaam:</label><br>
+    <input type="text" id="streetName" required><br>
+
+    <div id="map" style="height: 500px;"></div><br><br>
+    <label for="klacht" required>Type Klacht:</label><br>
+    <select name="klacht" id="klacht">
+        <option value="KiesKlacht" selected>- Kies uw klacht -</option>
+        <option value="Zwerfafval">Zwerfafval</option>
+        <option value="Dood_Dier">Dood dier</option>
+        <option value="Kapot_Stoplicht">Kapot Stoplicht</option>
+        <option value="Overig">Overig</option>
+    </select><br><br>
+
+    <label for="klachtText" required>Uw klacht:</label><br>
+    <textarea id="klachtText" rows="4" cols="50" required></textarea><br><br>
+
+    <input class="verzendknop" type="submit" value="Klacht Verzenden">
+</form>
+
 
 
         </form>
